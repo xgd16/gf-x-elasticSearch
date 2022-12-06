e301acec15fd1ed173cb68ef7d7a607642b34359
xgd16 <42709773+xgd16@users.noreply.github.com>
2022-12-06 14:38:38 +0800

    添加 helper AnyToIoReader

--- es/helper.go
+++ es/helper.go
@@ -2,10 +2,16 @@ package es
 
 import (
 	"bytes"
+	"github.com/gogf/gf/v2/encoding/gjson"
 	"io"
 )
 
 // StrToIoReader 将字符串转换为 io.Reader
 func StrToIoReader(s string) io.Reader {
 	return bytes.NewBuffer([]byte(s))
+}
+
+// AnyToIoReader 任何类型转换为 io.Reader
+func AnyToIoReader(d any) io.Reader {
+	return bytes.NewBuffer(gjson.MustEncode(d))
 }
